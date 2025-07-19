e3203dc51c59c80cbb69d315ce00c40f7e8f9342
DikaPutra28 <andikafadil60@gmail.com>
2025-07-19 17:01:55 +0700

    commit 3

--- index.php
+++ index.php
@@ -1,61 +1,16 @@
-<?php include 'head.php'; ?>
-
-
-<body id="page-top">
-
-    <!-- Page Wrapper -->
-    <div id="wrapper">
-
-        <!-- Sidebar -->
-        <?php include 'sidebar.php'; ?>
-
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
-
-            <!-- Main Content -->
-            <div id="content">
-
-                <!-- Topbar -->
-                <?php include 'head.php'; ?>
-                <?php include 'navbar.php'; ?>
-                <!-- End of Topbar -->
-                <!-- Conten -->
-                <?php
-                if (isset($_GET['x']) && $_GET['x'] == 'Home') {
-                    include "home.php";
-                } elseif (isset($_GET['x']) && $_GET['x'] == 'makanan') {
-                    include "makanan.php";
-                } elseif (isset($_GET['x']) && $_GET['x'] == 'minuman') {
-                    include "minuman.php";
-                }
-                ?>
-                <!-- Footer -->
-                <footer class="sticky-footer bg-white">
-                    <div class="container my-auto">
-                        <div class="copyright text-center my-auto">
-                            <span>Copyright &copy; Your Website 2021</span>
-                        </div>
-                    </div>
-                </footer>
-                <!-- End of Footer -->
-
-            </div>
-            <!-- End of Content Wrapper -->
-
-        </div>
-        <!-- End of Page Wrapper -->
-
-        <!-- Scroll to Top Button-->
-        <a class="scroll-to-top rounded" href="#page-top">
-            <i class="fas fa-angle-up"></i>
-        </a>
-
-        <!-- Logout Modal-->
-        
-
-        <?php include 'footerJS.php'; ?>
-
-
-</body>
-
-</html>
+<?php
+if (isset($_GET['x']) && $_GET['x'] == 'home') {
+    $page = "home.php";
+    include "main.php";
+} elseif (isset($_GET['x']) && $_GET['x'] == 'makanan') {
+    $page = 'makanan.php';
+    include "main.php";
+} elseif (isset($_GET['x']) && $_GET['x'] == 'minuman') {
+    $page = 'minuman.php';
+    include "main.php";
+} elseif (isset($_GET['x']) && $_GET['x'] == 'login') {
+    include "login.php";
+} else {
+    include "main.php";
+}
+?>
--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark  accordion" id="accordionSidebar">
 
       <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="home">
             <div class="sidebar-brand-icon rotate-n-15">
                   <i class="bi bi-fork-knife"></i>
             </div>
@@ -14,8 +14,7 @@
 
       <!-- Nav Item - Dashboard -->
       <li class="nav-item active">
-            <a class="nav-link <?php echo (isset($_GET['x']) && $_GET['x'] == 'Home') ? 'active link-light' : 'link-dark'; ?>"
-                  href="Home">
+            <a class="nav-link <?php echo (isset($_GET['x']) && $_GET['x'] == 'home') ?>" href="home">
                   <i class="fas fa-fw fa-tachometer-alt"></i>
                   <span>Home</span></a>
       </li>
@@ -38,9 +37,9 @@
             <div id="collapseOrder" class="collapse" aria-labelledby="headingOrder" data-parent="#accordionSidebar">
                   <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Pilihan</h6>
-                        <a class="nav-link  <?php echo (isset($_GET['x']) && $_GET['x'] == 'makanan') ? 'active link-light' : 'link-dark'; ?> collapse-item"
+                        <a class="nav-link collapse-item <?php echo (isset($_GET['x']) && $_GET['x'] == 'makanan') ?>"
                               href="makanan">Makanan</a>
-                        <a class="nav-link  <?php echo (isset($_GET['x']) && $_GET['x'] == 'minuman') ? 'active link-light' : 'link-dark'; ?> collapse-item"
+                        <a class="nav-link collapse-item <?php echo (isset($_GET['x']) && $_GET['x'] == 'minuman') ?> "
                               href="minuman">Minuman</a>
                   </div>
             </div>
